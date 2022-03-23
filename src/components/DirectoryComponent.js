ef359a6f7aea4f942cdcf46f17d13deb9e23f1d7
Travis Desktop <traviswilley24@yahoo.com>
2022-03-23 15:38:19 -0700

    React Redux forms revisited

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -17,7 +17,7 @@ function RenderDirectoryItem({campsite}) {
 }
 
 function Directory(props) {
-  const directory = props.campsites.csmpsites.map(campsite => {
+  const directory = props.campsites.campsites.map(campsite => {
     return (
       <div key={campsite.id} className="col-md-5 m-1">
         <RenderDirectoryItem campsite={campsite} />
