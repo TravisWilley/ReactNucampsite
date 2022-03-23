4c4fab3de8efffbfaf52dd7680b8bcb285a3213d
Travis Desktop <traviswilley24@yahoo.com>
2022-03-23 12:57:00 -0700

    Redux Thunk

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -16,6 +16,7 @@ import {
 } from 'reactstrap';
 import {Link} from 'react-router-dom';
 import {LocalForm, Control, Errors} from 'react-redux-form';
+import {Loading} from './LoadingComponent';
 
 const required = val => val && val.length;
 const maxLength = len => val => !val || val.length <= len;
@@ -149,31 +150,27 @@ function RenderComments({comments, addComment, campsiteId}) {
   return <div />;
 }
 function CampsiteInfo(props) {
-  if (props.campsite) {
+  if (props.isLoading) {
+    return (
+      <div className="container">
+        <div className="row">
+          <Loading />
+        </div>
+      </div>
+    );
+  }
+  if (props.errMess) {
     return (
       <div className="container">
         <div className="row">
           <div className="col">
-            <Breadcrumb>
-              <BreadcrumbItem>
-                <Link to="/directory">Directory</Link>
-              </BreadcrumbItem>
-              <BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
-            </Breadcrumb>
-            <h2>{props.campsite.name}</h2>
-            <hr />
+            <h4>{props.errMess}</h4>
           </div>
-        </div>
-        <div className="row">
-          <RenderCampsite campsite={props.campsite} />
-          <RenderComments
-            comments={props.comments}
-            addComment={props.addComment}
-            campsiteId={props.campsite.id}
-          />
         </div>
       </div>
     );
+  }
+  if (props.campsite) {
   }
   return <div />;
 }
