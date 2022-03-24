3be25fd132052557ccea5dfca43b32b03ace4111
Travis Desktop <traviswilley24@yahoo.com>
2022-03-23 17:23:11 -0700

    Fetch Handling Errors

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -17,8 +17,8 @@ import {
 import {Link} from 'react-router-dom';
 import {LocalForm, Control, Errors} from 'react-redux-form';
 import {Loading} from './LoadingComponent';
+import {baseUrl} from '../shared/baseUrl';
 
-const required = val => val && val.length;
 const maxLength = len => val => !val || val.length <= len;
 const minLength = len => val => val && val.length >= len;
 class CommentForm extends Component {
@@ -69,7 +69,6 @@ class CommentForm extends Component {
                   id="author"
                   className="form-control"
                   validators={{
-                    required,
                     minLength: minLength(2),
                     maxLength: maxLength(15),
                   }}
@@ -111,9 +110,8 @@ function RenderCampsite({campsite}) {
   return (
     <div className="col-md-5 m-1">
       <Card>
-        <CardImg top src={campsite.image} alt={campsite.name} />
+        <CardImg top src={baseUrl + campsite.image} alt={campsite.name} />
         <CardBody>
-          <CardTitle>{campsite.name}</CardTitle>
           <CardText>{campsite.description}</CardText>
         </CardBody>
       </Card>
